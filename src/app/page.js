12f1dce7f03065007db5e3f02dc016c8e7374f6a
박경서 <zerm0907@gmail.com>
2024-04-30 22:40:27 +0900

    db 연동 서버 컴포넌트

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <h2>Welcome</h2>
-      Hello, Web
-      <img src="/Tistory.svg" alt="티스토리 로고" />
+      <p>Hello, Web</p>
+      <Image width={100} height={100} src="/Tistory.svg" alt="티스토리 로고" />
     </>
   );
 }
