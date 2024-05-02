b696106b4ef9a2363b7152a105d36c14902788a7
zermzerm <zerm0907@gmail.com>
2024-05-02 18:01:57 +0900

    앱라우터 공부 2일차 캐싱까지

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,3 +1,42 @@
+"use client";
+
+//app 라우터에서는 useRouter를 next/navigation에서 가져온다
+import { useRouter } from "next/navigation";
+
 export default function Create() {
-  return <>Create!</>;
+  const router = useRouter();
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        fetch(`http://localhost:9999/topics`, options)
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result);
+            const lastId = result.id;
+            router.push(`/read/${lastId}`);
+            router.refresh();
+          });
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="title"></input>
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create"></input>
+      </p>
+    </form>
+  );
 }
